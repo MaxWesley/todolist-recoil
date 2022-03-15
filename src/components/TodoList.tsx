fe923a22ecd6db5bbd721e478e6f7d649492808e
MaxWesley <mx_wesley@hotmail.com>
2022-03-15 01:34:58 -0300

    feat: add filter to todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,58 @@
 import { 
   atom, 
+  selector, 
   useRecoilValue 
 } from "recoil"
+
 import { TodoItem } from "./TodoItem"
 import { TodoItemCreator } from "./TodoItemCreator"
+import { TodoListFilters } from "./TodoListFilters"
 
 const todoListState = atom<any[]>({
   key: 'todoListState',
-  default: []
+  default: [
+    {
+      id: 0,
+      text: 'Varrer a casa',
+      isComplete: false,
+    },
+    {
+      id: 1,
+      text: 'Estudar',
+      isComplete: true,
+    },
+  ]
+})
+
+const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All'
+})
+
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState)
+    const list = get(todoListState)
+
+    switch(filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete);
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete);
+      default: 
+        return list;
+    }
+  }
 })
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState)
+  const todoList = useRecoilValue(filteredTodoListState)
 
   return(
     <>
       {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem: any) => (
@@ -28,5 +64,6 @@ function TodoList() {
 
 export { 
   TodoList,
-  todoListState
+  todoListState,
+  todoListFilterState
 }
